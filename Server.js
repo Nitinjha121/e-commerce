13ca66c6991f8f2ee738f026edce6bde352ea922
Nitin <nitinjha5121@gmail.com>
2021-03-25 20:30:51 +0530

    Secondd commit

--- Server.js
+++ Server.js
@@ -4,56 +4,25 @@ const path = require("path");
 const cors = require("cors");
 const { Product } = require("./database");
 const { User } = require("./database");
-const bcrypt = require("bcrypt");
-const passport = require("passport");
-// const initializePassport = require("./passport-config");
-const flash = require("express-flash");
-// const session = require("express-session");
 require("dotenv/config");
 
 const port = process.env.PORT || 4000;
-
-// initializePassport(
-//   passport,
-//   async (email) => await User.find({ email: email }).exec(),
-//   async (id) => await User.findById(id).exec()
-// );
 
 // Middlewares
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(flash());
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET_KEY,
-//     resave: false,
-//     saveUninitialized: false,
-//   })
-// );
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Inserting data
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-app.post(
-  "user/login",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-    failureFlash: true,
-  })
-);
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 app.post("/user/register", async (req, res) => {
   try {
@@ -65,8 +34,6 @@ app.post("/user/register", async (req, res) => {
         throw new Error("User already registered");
       }
     });
-
-    const hashPassword = await bcrypt.hash(req.body.password, 10);
 
     const user = new User({
       name: req.body.name,
@@ -91,10 +58,9 @@ app.get("/api/products", async (req, res) => {
   }
 });
 
-// app.get("/api/products", (req, res) => {});
-app.get("/products/:id", (req, res) => {
-  res.sendFile(__dirname + "/build/index.html");
-});
+// app.get("/products/:id", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 app.post("/products");
 
@@ -102,11 +68,11 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
 
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 }
 
