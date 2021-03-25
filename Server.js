4befafcbadef49bec93a84a0af8369c4202719e5
Nitin <nitinjha5121@gmail.com>
2021-03-25 17:19:54 +0530

    Secondd commit

--- Server.js
+++ Server.js
@@ -9,7 +9,7 @@ const passport = require("passport");
 // const initializePassport = require("./passport-config");
 const flash = require("express-flash");
 // const session = require("express-session");
-// require("dotenv/config");
+require("dotenv/config");
 
 const port = process.env.PORT || 4000;
 
