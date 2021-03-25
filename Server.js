c7aa8578d5f99a3d774face97108605f798a55c6
Nitin <nitinjha5121@gmail.com>
2021-03-25 10:51:56 +0530

    add environment variable

--- Server.js
+++ Server.js
@@ -9,7 +9,7 @@ const passport = require("passport");
 // const initializePassport = require("./passport-config");
 const flash = require("express-flash");
 // const session = require("express-session");
-// require("dotenv/config");
+require("dotenv/config");
 
 // initializePassport(
 //   passport,
@@ -73,7 +73,6 @@ app.post("/user/register", async (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  console.log(Product, User, "");
   res.sendFile(__dirname + "/build/index.html");
 });
 
